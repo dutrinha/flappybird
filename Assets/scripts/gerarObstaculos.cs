8b64d31fec2138b6e3b15028b104408500b01891
dutrinha <kickjp123@gmail.com>
2024-05-13 11:25:59 -0300

    gerador de obstaculos

--- Assets/scripts/gerarObstaculos.cs
+++ Assets/scripts/gerarObstaculos.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class gerarObstaculos : MonoBehaviour
 {
-
-
-
     // como criar os obstaculos?
     // onde criar?
     // quando criar?
 
+    [SerializeField]
+    private float tempoParaGerar = 3;
+
+    private float cronometro;
+
+    [SerializeField]
+    private GameObject modeloObstaculo;
+
+    private void Awake()
+    {
+        this.cronometro = this.tempoParaGerar;
+    }
+
     void Update()
     {
-        
+        this.cronometro -= Time.deltaTime;
+        if(this.cronometro < 0)
+        {
+            GameObject.Instantiate(this.modeloObstaculo, this.transform.position, Quaternion.identity);
+            this.cronometro = this.tempoParaGerar;
+        }
     }
 }
