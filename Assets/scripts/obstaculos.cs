ae4f2b5fe229e3d06b3f47854a42d7d8f1182f74
dutrinha <kickjp123@gmail.com>
2024-05-27 11:13:55 -0300

    Update obstaculos.cs

--- Assets/scripts/obstaculos.cs
+++ Assets/scripts/obstaculos.cs
@@ -7,6 +7,14 @@ public class obstaculos : MonoBehaviour
     [SerializeField]
     private float velocidade = 2f;
 
+    [SerializeField]
+    private float variacaoposicaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(variacaoposicaoY,variacaoposicaoY));
+    }
+
     // Update is called once per frame
     void Update()
     {
