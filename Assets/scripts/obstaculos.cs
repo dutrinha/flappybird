17cc4500ab4cd70803317027b21e455b37091a4f
dutrinha <kickjp123@gmail.com>
2024-06-10 11:31:37 -0300

    pontuacao

--- Assets/scripts/obstaculos.cs
+++ Assets/scripts/obstaculos.cs
@@ -9,17 +9,37 @@ public class obstaculos : MonoBehaviour
 
     [SerializeField]
     private float variacaoposicaoY;
+    private Vector3 posicaoPassaro;
+    private UIController controladorUI;
+
+    private bool pontuei;
 
     private void Awake()
     {
-        this.transform.Translate(Vector3.up * Random.Range(variacaoposicaoY,variacaoposicaoY));
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY,variacaoposicaoY));
+    }   
+    private void Start()
+    {
+        this.posicaoPassaro = GameObject.FindObjectOfType<passaro>().transform.position;
+        this.controladorUI = GameObject.FindObjectOfType<UIController>();
     }
-
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+        if (!this.pontuei && this.transform.position.x < posicaoPassaro.x) 
+        {
+            this.controladorUI.adicionarPontos();
+            this.pontuei = true;
+        }
 
-
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+    private void Destruir()
+    {
+        Destroy(this.gameObject);
     }
 }
